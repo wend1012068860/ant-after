08629b7b74cd1652cf069f8f150a6e8b9e884488
1012068860@qq.com <1012068860@qq.com>
2022-09-14 11:13:49 +0800

    Initial commit

---
+++ src/main/java/com/example/antafter/AntAfterApplication.java
@@ -0,0 +1,13 @@
+package com.example.antafter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntAfterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntAfterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/antafter/AntAfterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.antafter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntAfterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
